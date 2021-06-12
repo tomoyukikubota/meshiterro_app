01c3cff49cda9ad9af0181f0358319c489111d19
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-06-12 08:50:21 +0000

    [Finish]4-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -27,7 +27,7 @@ class PostImagesController < ApplicationController
   end
 
 
-   private
+  private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
